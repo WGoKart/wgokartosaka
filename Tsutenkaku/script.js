6a62589d7f1d1202b978e570f4cd681255aee452
吳峻葳 <evan@wujunweideMacBook-Air.local>
2026-06-22 15:50:32 +0900

    fix safari date limit

--- Tsutenkaku/script.js
+++ Tsutenkaku/script.js
@@ -33,7 +33,7 @@ function hidePopup(){
 
 }
 
-document.querySelector(".map").addEventListener("click",(e)=>{
+/*document.querySelector(".map").addEventListener("click",(e)=>{
 
     const rect = e.currentTarget.getBoundingClientRect();
 
@@ -45,4 +45,4 @@ document.querySelector(".map").addEventListener("click",(e)=>{
 
     alert(`top:${top}%; left:${left}%;`);
 
-});
+});*/
